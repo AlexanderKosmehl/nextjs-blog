2edfb79f7597045a7943284d2f0eee31b66c0ceb
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-08-24 17:24:17 +0200

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+import '../styles/global.css'
+
+export default function App ({ Component, pageProps }) {
+  return (
+    <Component {...pageProps} />
+  )
+}
